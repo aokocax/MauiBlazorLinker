67bc66c91cbfc321ae7bb8af50c3b5f9cb258f7b
Ahmet Oğuz Koca <aoguzkoca@hotmail.com>
2022-01-31 16:54:23 +0300

    Add project files.

---
+++ MauiBlazorLinker/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBlazorLinker;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiBlazorLinker/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorLinker;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
